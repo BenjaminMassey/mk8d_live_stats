e2e920568e176d3e8b01d65e8254be42482b40ed
Benjamin Massey <benjamin.w.massey@gmail.com>
2024-07-11 20:33:00 -0700

    Properly doing subimage calculation for image spots

--- src/image_proc.rs
+++ src/image_proc.rs
@@ -2,13 +2,24 @@ use crate::data;
 
 use std::str::FromStr;
 
+const FIRST_SPOT: ((f64, f64), (f64, f64)) = ((0.09619, 0.10112), (0.07441, 0.13804));
+const SECOND_SPOT: ((f64, f64), (f64, f64)) = ((0.04719, 0.06581), (0.03902, 0.06421));
+
 pub fn get_items(full_shot: &image::ImageBuffer<image::Rgba<u8>, Vec<u8>>) -> data::Items {
     let image_one = image::SubImage::new(
-        full_shot, 106, 63, 82, 86,
-    ).to_image(); // TODO: dynamically calculated pos/size
+        full_shot,
+        (FIRST_SPOT.0.0 * full_shot.width() as f64) as u32,
+        (FIRST_SPOT.0.1 * full_shot.height() as f64) as u32,
+        (FIRST_SPOT.1.0 * full_shot.width() as f64) as u32,
+        (FIRST_SPOT.1.1 * full_shot.height() as f64) as u32,
+    ).to_image();
     let image_two = image::SubImage::new(
-        full_shot, 52, 41, 43, 40,
-    ).to_image(); // TODO: dynamically calculated pos/size
+        full_shot,
+        (SECOND_SPOT.0.0 * full_shot.width() as f64) as u32,
+        (SECOND_SPOT.0.1 * full_shot.height() as f64) as u32,
+        (SECOND_SPOT.1.0 * full_shot.width() as f64) as u32,
+        (SECOND_SPOT.1.1 * full_shot.height() as f64) as u32,
+    ).to_image();
     let item_choices = std::fs::read_dir("./icons/items/")
         .unwrap()
         .map(|f| f.unwrap().path().to_owned())
